13b5e7507ca030a11b65b19cbb6349801560b226
Cookjp <james.cook@motionmetrics.co>
2023-12-27 17:32:07 +0000

    try this config

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,18 @@ import react from "@vitejs/plugin-react";
 import fs from "vite-plugin-fs";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {  
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
   return {
     define: {
-      'process.env': env,
+      "typeof process !== 'undefined'": "true",
     },
     plugins: [
       react({
-      include: "**/*.tsx",
+        include: "**/*.tsx",
       }),
       fs(),
     ],
     publicDir: "./public",
-  }
+  };
 });
