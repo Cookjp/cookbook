b2192511e238420d075789ebc354d5bcf01cfd77
Cookjp <james.cook@motionmetrics.co>
2023-12-27 18:12:08 +0000

    debug logging

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,5 +1,6 @@
 import { createClient } from "@vercel/kv";
 
+console.log(import.meta.env)
 export default createClient({
   url: import.meta.env.VITE_KV_REST_API_URL,
   token: import.meta.env.VITE_KV_REST_API_TOKEN,
