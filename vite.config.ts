60c13f7d9d4a76dcee774c96f1ff9d479bb95e91
Cookjp <james.cook@motionmetrics.co>
2023-12-27 19:04:45 +0000

    set UPSTASH_DISABLE_TELEMETRY in prod

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,15 @@ import fs from "vite-plugin-fs";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const define = {};
-  if (mode === "development") {
+  const define = {}
+  if(mode === 'development') {
     const env = loadEnv(mode, process.cwd(), "");
-    define["process.env"] = env;
+    define['process.env'] = env
+  }
+  console.log('process',  process)
+  if(mode === 'production') {
+    define['process.env.UPSTASH_DISABLE_TELEMETRY'] = "1"
+    // define['process.env'] = { UPSTASH_DISABLE_TELEMETRY: "1"}
   }
 
   return {
